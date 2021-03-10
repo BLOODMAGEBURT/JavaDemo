a25efc476c986735f4649704e48fdab56f746af6
xujianbo <henghenghalala@163.com>
2021-03-11 00:51:21 +0800

    Factory

---
+++ src/com/burt/pattern/factory/Main.java
@@ -0,0 +1,16 @@
+package com.burt.pattern.factory;
+
+/**
+ * Main
+ * 工厂模式
+ *
+ * @author xujianbo
+ * @date 2021/3/11 12:33 上午
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
